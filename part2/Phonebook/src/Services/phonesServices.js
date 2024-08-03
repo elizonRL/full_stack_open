5684155325e382b6fb56429d29ac58b48d1163c6
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2024-08-02 21:50:30 -0400

    part 2 add metho put

--- part2/Phonebook/src/Services/phonesServices.js
+++ part2/Phonebook/src/Services/phonesServices.js
@@ -8,13 +8,15 @@ const getAll = () => {
 };
 const deletePerson = (id) => {
   const request = axios.delete(`${API_URL}/${id}`);
-  return request.then((response) => {
-    response.data;
-   
-  });
+  return request.then((response) => response.data);
 };
+const editPerson = (id)=>{
+  const request = axios.put(`${API_URL}/${id}`);
+  return request.then(response => response.data);
+}
 
 export default {
   getAll,
   deletePerson,
+  editPerson
 };
