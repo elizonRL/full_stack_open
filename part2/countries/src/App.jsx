b6973d85fb4a5cd4a37397df6432478bfcde1016
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2024-08-06 17:26:08 -0400

    chamge

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 function App() {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState("");
+ /*  const [country, setCountry] = useState([]); */
   useEffect(() => {
     axios
       .get("https://studies.cs.helsinki.fi/restcountries/api/all")
@@ -16,11 +17,38 @@ function App() {
   );
   console.log("countries to show", countriesToShow);
   const findCountry = () => {
-    if(countriesToShow.length === 1){
-        return countriesToShow[0]
+    if(search === "") {
+      return null
     }
+    if (countriesToShow.length > 10) {
+      return "Too many matches, specify another filter";
+    }
+    if (countriesToShow.length > 1 && countriesToShow.length <= 10) {
+      return countriesToShow.map((country) => (
+        <li key={country.name.common}>{country.name.common}</li>
+      ));
+    }
+    if (countriesToShow.length === 1) {
+      return (
+        <div>
+          <h2>{countriesToShow[0].name.common}</h2>
+          <p>capital {countriesToShow[0].capital}</p>
+          <p>area {countriesToShow[0].area}</p>
+          <h3>languages:</h3>
+          <ul>
+            {Object.values(countriesToShow[0].languages).map((language) => (
+              <li key={language}>{language}</li>
+            ))}
+          </ul>
+          <img src={countriesToShow[0].flags.png} alt="flag" />
+        </div>
+      );
+    }
+    /* if (countriesToShow.length === 1) {
+      return countriesToShow[0];
+    } */
   };
-  console.log("counties -->",countriesToShow);
+  console.log("counties -->", countriesToShow);
   return (
     <>
       <h1>Countries</h1>
@@ -31,17 +59,7 @@ function App() {
         onChange={(event) => setSearch(event.target.value)}
       />
       <ul>
-        {findCountry() ? (
-          <li key={findCountry().name.common}>
-            {findCountry().name.common} {findCountry().capital}
-          </li>
-        ) : (
-          countriesToShow.map((country) => (
-            <li key={country.name.common}>
-              {country.name.common} {country.capital}
-            </li>
-          ))
-        )}
+        {findCountry()}
       </ul>
     </>
   );
